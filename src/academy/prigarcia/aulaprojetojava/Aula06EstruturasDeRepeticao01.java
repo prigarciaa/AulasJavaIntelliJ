e6c421e681766295b6e2a8593191e66f08bebb53
prigarciaa <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>
2025-07-10 10:00:04 -0300

    Aula06EstruturasDeRepeticao01

---
+++ src/academy/prigarcia/aulaprojetojava/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,18 @@
+package academy.prigarcia.aulaprojetojava;
+
+public class Aula06EstruturasDeRepeticao {
+    public static void main(String[] args) {
+        //while, do while, for
+        int count = 12;
+        while (count < 10) {
+            System.out.println(++count);
+        }
+        do {
+            System.out.println("Dentro do do-while");
+        } while (count < 10);
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println("For " + i);
+        }
+    }
+}
